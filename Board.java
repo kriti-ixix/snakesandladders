e6477e9df9cc1b5a108f0ed7beb8a6c946c1b9bf
Kriti Bhatia <60443387+kriti-ixix@users.noreply.github.com>
2021-02-20 19:58:31 +0530

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,150 @@
+package com.example.helloworld;
+
+import java.util.ArrayList;
+
+public class Board
+{
+    //Array of square objects
+    Square[] boardSquares;
+    int rows, columns;
+    //Arraylist to store players
+    ArrayList<Player> playersList = new ArrayList<>();
+
+    //Constructor of the Board class
+    Board(int rows, int columns)
+    {
+        this.rows = rows; this.columns = columns;
+        boardSquares = new Square[rows*columns];
+        for(int i = 0 ; i < rows * columns ; i++)
+        {
+            Square square = new Square(i);
+            boardSquares[i] = square;
+        }
+    }
+
+    //toString method to return entire board
+    public String toString()
+    {
+        String boardString = boardSquares.toString();
+        return boardString;
+    }
+
+    //Helper method to return row and column of a row
+
+    //Method to add players
+    public void addPlayer(Player playerName)
+    {
+        playersList.add(playerName);
+        boardSquares[playerName.playerSquare.position].setPlayer(playerName);
+    }
+
+    //Method to return reference to the mentioned Square object
+    public Square getSquareInfo(int pos)
+    {
+        return boardSquares[pos];
+    }
+
+    //takeTurns method to loop through all the players and calls their move
+    public boolean takeTurns()
+    {
+        int i=0; boolean win=false;
+        //If player wins, this terminates
+        while (!win)
+        {
+            while (i < playersList.size())
+            {
+                win = playersList.get(i).move();
+                printBoard();
+                if(win)
+                {
+                    return true;
+                }
+                i++;
+            }
+            i = 0;
+        }
+        return win;
+    }
+
+    public void printBoard()
+    {
+        /*
+        //Creating 2d array to print out the board
+        Square[][] boardMatrix = new Square[rows][columns];
+
+        //To iterate over squares in reverse
+        int i = boardSquares.length-1;
+        //System.out.println("Total: " + i);
+
+        //Coverting 1d boardSquares array into a 2d matrix
+        for (int row=0; row<=9; row++)
+        {
+            if (row%2==0)
+            {
+                //System.out.println("\nEven row: " + row);
+                //Even number of rows in ascending order
+                for (int col = 0; col<5; col++)
+                {
+                    //System.out.print("Ascending" + col + ' ');
+                    boardMatrix[row][col] = boardSquares[i--];
+                }
+            }
+            else
+            {
+                //System.out.println("\nOdd row: " + row);
+                //Odd number of rows in descending order
+                for (int col = 4; col>-1; col--)
+                {
+                    //System.out.print("Descending" + col + ' ');
+                    boardMatrix[row][col] = boardSquares[i--];
+                }
+            }
+        }
+
+        //Printing out the board
+        for (int r=0; r<10; r++)
+        {
+            for (int c=0; c<5; c++)
+            {
+                System.out.print(boardMatrix[r][c]);
+            }
+            System.out.println();
+        }
+
+         */
+
+        for (int i=49; i>=0; i--)
+        {
+
+            if (i%5==0)
+            {
+                System.out.print(boardSquares[i]);
+                System.out.println(' ');
+            }
+            else
+            {
+                System.out.print(boardSquares[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        //Creating board with 10 rows and 5 columns using the class constructor
+        Board board = new Board(10, 5);
+
+        //Creating players and adding them to the arraylist
+        Player ePlayer = new Player('E');
+        board.addPlayer(ePlayer);
+        Player sPlayer = new Player('S');
+        board.addPlayer(sPlayer);
+
+        //Setting players at the starting position: Square 0
+        ePlayer.setPlayerSquare(board.boardSquares[0]);
+        sPlayer.setPlayerSquare(board.boardSquares[0]);
+
+        //Print the board
+        board.printBoard();
+    }
+
+}
---
+++ Player.java
@@ -0,0 +1,70 @@
+package com.example.helloworld;
+
+import java.util.Random;
+
+public class Player
+{
+    char playerIdentifier;
+    //playerIdentifier in this example is either E or S
+
+    //Reference to square
+    Square playerSquare;
+
+    Player(char playerIdentifier)
+    {
+        this.playerIdentifier = playerIdentifier;
+    }
+
+    void setPlayerSquare(Square playerSquare)
+    {
+        this.playerSquare = playerSquare;
+    }
+
+    //toString method to return the player character
+    public String toString()
+    {
+        return String.valueOf(playerIdentifier);
+    }
+
+    //Move method to check if the player's move lead to a win or not
+    public boolean move()
+    {
+        //Rolling a die and moving according to delta value
+        Random r = new Random();
+        int count = r.nextInt(6)+1; //To get values between 1 and 6
+
+        //Updates player's position according to the dice value
+        if(playerSquare.position + count > 49)
+        {
+            return false;
+        }
+
+        playerSquare.position += count;
+        System.out.println("\nComputer player's positon: "+playerSquare.position);
+        if (playerSquare.delta!=0)
+        {
+            //Updates value in case of a delta value on the square
+            playerSquare.position += playerSquare.delta;
+        }
+
+        //Checking player's final position after moving
+        if (playerSquare.position == 49)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    //main method
+    //Creation of player and square
+    public static void main(String[] args) {
+        //Create an arraylist of player
+        Player pe = new Player('E');
+        Player ps = new Player('S');
+    }
+}
+
+
